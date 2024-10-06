a1a79b362d7bb72e184f4878f418351420e2aa43
StephXius <stephanexius1@gmail.com>
2024-10-06 18:31:55 -0400

    fourth push

--- main/script.js
+++ main/script.js
@@ -1,14 +1,15 @@
 let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
+window.onload = function() {
+  showSlides(slideIndex); // Show first slide on load
+};
+
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+  showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
@@ -16,26 +17,17 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
 
-  // If n exceeds the total number of slides, go back to the first slide
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  // If n is less than 1, go to the last slide
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
 
-  // Hide all slides by default
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
-  // Remove the "active" class from all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
 
-  // Display the current slide and mark the current dot as active
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
